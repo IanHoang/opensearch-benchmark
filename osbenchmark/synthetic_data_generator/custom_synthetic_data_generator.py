7d25fafa9b6b61ed57106218029168e5608b3d8f
Ian Hoang <ianhoang16@gmail.com>
2025-04-03 16:56:09 -0500

    Remove load_config from custom_synthetic_data_generator.py
    Signed-off-by: Ian Hoang <ianhoang16@gmail.com>

--- osbenchmark/synthetic_data_generator/custom_synthetic_data_generator.py
+++ osbenchmark/synthetic_data_generator/custom_synthetic_data_generator.py
@@ -30,13 +30,6 @@ def load_user_module(file_path):
     spec.loader.exec_module(user_module)
     return user_module
 
-def load_config(config_path):
-    if config_path.endswith('.yml') or config_path.endswith('.yaml'):
-        with open(config_path, 'r') as file:
-            return yaml.safe_load(file)
-    else:
-        return {}
-
 def write_chunk(data, file_path):
     with open(file_path, 'a') as f:
         for item in data:
