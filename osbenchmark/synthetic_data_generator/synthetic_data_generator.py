898ec7aa6876f0586885197a21d079fca08b2915
Ian Hoang <ianhoang16@gmail.com>
2025-04-15 13:29:38 -0500

    Split up the main orchestration into two separate functions
    Signed-off-by: Ian Hoang <ianhoang16@gmail.com>

--- osbenchmark/synthetic_data_generator/synthetic_data_generator.py
+++ osbenchmark/synthetic_data_generator/synthetic_data_generator.py
@@ -27,26 +27,11 @@ from osbenchmark.synthetic_data_generator.helpers import load_config, write_reco
 from osbenchmark.synthetic_data_generator.types import DEFAULT_MAX_FILE_SIZE_GB, DEFAULT_CHUNK_SIZE
 from osbenchmark.synthetic_data_generator import custom_synthetic_data_generator, mapping_synthetic_data_generator
 
-def orchestrate_data_generation(cfg):
+def orchestrate_data_generation_for_custom_synthetic_data_generator(cfg, sdg_config, custom_config, dask_client):
     logger = logging.getLogger(__name__)
-    sdg_config = create_sdg_config_from_args(cfg)
+    logger.info("Generating data with custom synthetic data generator")
 
-    # TODO: Rename custom config
-    custom_config = load_config(sdg_config.custom_config_path)
-
-    # TODO: Move client creation to outside of orchestrator so that synthetic data generators can call on it
-    workers = custom_config.get("workers", os.cpu_count())
-    dask_client = Client(n_workers=workers, threads_per_worker=1)  # We keep it to 1 thread because generating random data is CPU intensive
-    blueprint = sdg_config.blueprint
-    logger.info("Number of workers to use: %s", workers)
-    logger.info("Blueprint: %s", json.dumps(blueprint, indent=2))
-
-    console.println(f"[NOTE] Dashboard link to monitor processes and task streams: {dask_client.dashboard_link}")
-    console.println("[NOTE] For users who are running generation on a virtual machine, consider tunneling to localhost to view dashboard.")
-    console.println("")
-
-    # TODO: Move to two separate functions
-    if use_custom_synthetic_data_generator(sdg_config) and cfg.opts("synthetic_data_generator", "test_document"):
+    if cfg.opts("synthetic_data_generator", "test_document"):
         custom_module = custom_synthetic_data_generator.load_user_module(sdg_config.custom_module_path)
         generate_fake_document = custom_module.generate_fake_document
         custom_module_components = custom_config.get('CustomSyntheticDataGenerator', {})
@@ -61,16 +46,20 @@ def orchestrate_data_generation(cfg):
         console.println("Please verify that the output is generated as intended. \n")
         print(json.dumps(document, indent=2))
 
-    elif use_custom_synthetic_data_generator(sdg_config):
+    else:
+        # Generate all documents
         custom_module = custom_synthetic_data_generator.load_user_module(sdg_config.custom_module_path)
 
         print("Starting generation")
-        # Generate all documents
         total_time_to_generate_dataset, generated_dataset_details = custom_synthetic_data_generator.generate_dataset_with_user_module(dask_client, sdg_config, custom_module, custom_config)
 
         write_record_and_publish_summary_to_console(sdg_config, total_time_to_generate_dataset, generated_dataset_details)
 
-    elif use_mappings_synthetic_data_generator(sdg_config) and cfg.opts("synthetic_data_generator", "test_document"):
+def orchestrate_data_generation_for_mapping_synthetic_data_generator(cfg, sdg_config, dask_client):
+    logger = logging.getLogger(__name__)
+    logger.info("Generating data with mapping synthetic data generator")
+
+    if cfg.opts("synthetic_data_generator", "test_document"):
         # TODO Remove config from this meethod and just load it in the beginning
         raw_mappings, mapping_config = mapping_synthetic_data_generator.load_mapping_and_config(sdg_config.index_mappings_path, sdg_config.custom_config_path)
         document = mapping_synthetic_data_generator.generate_test_document(raw_mappings, mapping_config)
@@ -78,7 +67,7 @@ def orchestrate_data_generation(cfg):
         console.println("Generating a single test document:")
         console.println("Please verify that the output is generated as intended. \n")
         print(json.dumps(document, indent=2))
-    elif use_mappings_synthetic_data_generator(sdg_config):
+    else:
         raw_mappings, mapping_config = mapping_synthetic_data_generator.load_mapping_and_config(sdg_config.index_mappings_path, sdg_config.custom_config_path)
 
         docs_written, total_time_to_generate_dataset, dataset_size = mapping_synthetic_data_generator.generate_dataset_with_mappings(dask_client, sdg_config, raw_mappings, mapping_config)
@@ -94,3 +83,24 @@ def orchestrate_data_generation(cfg):
         logger.info("Visit the following path to view synthetically generated data: [%s]", sdg_config.output_path)
         console.println(f"Visit the following path to view synthetically generated data: {sdg_config.output_path}")
 
+
+def orchestrate_data_generation(cfg):
+    logger = logging.getLogger(__name__)
+    sdg_config = create_sdg_config_from_args(cfg)
+
+    # TODO: Rename custom config
+    custom_config = load_config(sdg_config.custom_config_path)
+
+    # TODO: Move client creation to outside of orchestrator so that synthetic data generators can call on it
+    workers = custom_config.get("settings", {}).get("workers", os.cpu_count())
+    dask_client = Client(n_workers=workers, threads_per_worker=1)  # We keep it to 1 thread because generating random data is CPU intensive
+    logger.info("Number of workers to use: %s", workers)
+
+    console.println(f"[NOTE] Dashboard link to monitor processes and task streams: {dask_client.dashboard_link}")
+    console.println("[NOTE] For users who are running generation on a virtual machine, consider tunneling to localhost to view dashboard.")
+    console.println("")
+
+    if use_custom_synthetic_data_generator(sdg_config):
+        orchestrate_data_generation_for_custom_synthetic_data_generator(cfg, sdg_config, custom_config, dask_client)
+    elif use_mappings_synthetic_data_generator(sdg_config):
+        orchestrate_data_generation_for_mapping_synthetic_data_generator(cfg, sdg_config, dask_client)
